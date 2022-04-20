1afd180c639fea181ab6c75563a66571bc01f1ab
come_ill_foo <rayveleaveme34@inbox.ru>
2022-04-20 19:31:06 +0300

    feat: print binary and circular shift left

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+static int cshl( int n, int shift ) {
+  return ( n << shift ) | ( n >> ( sizeof( n ) * 8 - shift ) );
+}
+
+static void print_bin( int n ) {
+  for ( size_t i = 0; i < sizeof( n ) * 8; ++i )
+    printf( "%d", cshl( n, i + 1 ) & 1 );
+  printf( "\n" );
+}
+
 int main( int argc, char** argv ) {
-  printf( "Hello, World!\n" );
+  int n = 0;
+  scanf( "%d", &n ); // read the random int
+  
+  print_bin( n ); // print binary represenation
+
   return 0;
 }
